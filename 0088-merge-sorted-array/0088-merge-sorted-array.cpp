7abdf08c19c6330238d5aad27e32c1db38409d32
Connor Nguyen <88961694+connornguyen@users.noreply.github.com>
2023-07-22 14:29:29 +0700

    Time: 0 ms (100.00%), Space: 9 MB (79.70%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,10 +2,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k;
+       /* int k;
         if(nums1.size()== 0 || nums2.size() == 0){
             return;
-        } 
+        } */
 
         for(int i =0;i < n ;i++){
             nums1.pop_back();
