87f3d767117f6a5c0625eec5636bda183980d982
Connor Nguyen <88961694+connornguyen@users.noreply.github.com>
2023-07-28 10:53:06 +0700

    Time: 0 ms (100.00%), Space: 8.4 MB (88.01%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> ans;
+        //vector<int> ans;
         int sum = 0;
         for(int i = 0; i < nums.size(); i++){
             sum = sum + nums[i];
-            ans.push_back(sum);
+            nums[i] = sum;
         }
-        return ans;
+        return nums;
     }
 };
