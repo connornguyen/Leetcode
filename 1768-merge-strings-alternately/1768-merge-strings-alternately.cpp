bfd20e4c1b5f1b42ead01ec9aaf32b9b53d61662
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2023-09-04 14:49:54 -0700

    Time: 4 ms (20.46%), Space: 6.2 MB (85.54%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int cur, n;
-        if(word1.length() > word2.length()) n = word1.length();
-        else n = word2.length();
-        string ans;
-        for(int i = 0; i < n; ++i){
+        string ans = "";
+        int i = 0;
+        while (i < word1.length() || i < word2.length()){
             if(i < word1.length())ans += word1[i];
             if(i < word2.length())ans += word2[i];
+            i++;
         }
         return ans;
     }
